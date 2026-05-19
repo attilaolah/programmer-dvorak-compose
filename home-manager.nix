d3d7e56f145dde40286469c7c5cf99ca03dc34a9
Attila Oláh <attila@dorn.haus>
2026-05-19 22:59:00 +0200

    feat: package keyboard layout icon

--- home-manager.nix
+++ home-manager.nix
@@ -5,10 +5,12 @@ self: {
 }: {
   home.file = lib.optionalAttrs pkgs.stdenv.isDarwin (let
     package = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    bundle = "${package}/Library/Keyboard Layouts/Programmer Dvorak Compose.bundle";
   in {
-    "Library/Keyboard Layouts/Programmer Dvorak Compose.bundle/Contents/Info.plist".source = "${package}/info.plist";
-    "Library/Keyboard Layouts/Programmer Dvorak Compose.bundle/Contents/version.plist".source = "${package}/version.plist";
-    "Library/Keyboard Layouts/Programmer Dvorak Compose.bundle/Contents/Resources/English.lproj/InfoPlist.strings".source = "${package}/info_plist.strings";
-    "Library/Keyboard Layouts/Programmer Dvorak Compose.bundle/Contents/Resources/Programmer Dvorak Compose.keylayout".source = "${package}/programmer_dvorak_compose.keylayout";
+    "Library/Keyboard Layouts/Programmer Dvorak Compose.bundle/Contents/Info.plist".source = "${bundle}/Contents/Info.plist";
+    "Library/Keyboard Layouts/Programmer Dvorak Compose.bundle/Contents/version.plist".source = "${bundle}/Contents/version.plist";
+    "Library/Keyboard Layouts/Programmer Dvorak Compose.bundle/Contents/Resources/English.lproj/InfoPlist.strings".source = "${bundle}/Contents/Resources/English.lproj/InfoPlist.strings";
+    "Library/Keyboard Layouts/Programmer Dvorak Compose.bundle/Contents/Resources/Programmer Dvorak Compose.keylayout".source = "${bundle}/Contents/Resources/Programmer Dvorak Compose.keylayout";
+    "Library/Keyboard Layouts/Programmer Dvorak Compose.bundle/Contents/Resources/Programmer Dvorak Compose.icns".source = "${bundle}/Contents/Resources/Programmer Dvorak Compose.icns";
   });
 }
--- nix-darwin.nix
+++ nix-darwin.nix
@@ -9,17 +9,18 @@ self: {
     system.activationScripts.programmer-dvorak-compose.text = ''
       set -eu
 
-      src="${package}"
+      src="${package}/Library/Keyboard Layouts/Programmer Dvorak Compose.bundle"
       dst="/Library/Keyboard Layouts/Programmer Dvorak Compose.bundle"
       tmp="/Library/Keyboard Layouts/.Programmer Dvorak Compose.bundle.tmp.$$"
 
       rm -rf "$tmp"
       mkdir -p "$tmp/Contents/Resources/English.lproj"
 
-      install -m 0644 "$src/info.plist" "$tmp/Contents/Info.plist"
-      install -m 0644 "$src/version.plist" "$tmp/Contents/version.plist"
-      install -m 0644 "$src/info_plist.strings" "$tmp/Contents/Resources/English.lproj/InfoPlist.strings"
-      install -m 0644 "$src/programmer_dvorak_compose.keylayout" "$tmp/Contents/Resources/Programmer Dvorak Compose.keylayout"
+      install -m 0644 "$src/Contents/Info.plist" "$tmp/Contents/Info.plist"
+      install -m 0644 "$src/Contents/version.plist" "$tmp/Contents/version.plist"
+      install -m 0644 "$src/Contents/Resources/English.lproj/InfoPlist.strings" "$tmp/Contents/Resources/English.lproj/InfoPlist.strings"
+      install -m 0644 "$src/Contents/Resources/Programmer Dvorak Compose.keylayout" "$tmp/Contents/Resources/Programmer Dvorak Compose.keylayout"
+      install -m 0644 "$src/Contents/Resources/Programmer Dvorak Compose.icns" "$tmp/Contents/Resources/Programmer Dvorak Compose.icns"
 
       chown -R root:wheel "$tmp"
       chmod -R u=rwX,go=rX "$tmp"
